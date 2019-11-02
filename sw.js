e63e6289397d55104a929537a008bae9e5daeb47
luonghuynh <luonghuynh2525@gmail.com>
2019-11-02 16:02:02 +0700

    commit file len

---
+++ sw.js
@@ -0,0 +1,35 @@
+var cacheName = 'hello-pwa-2';
+var filesToCache = [
+  '/',
+  './index.html',
+  './assets/css/main.css',
+  './assets/css/main.css.map',
+  './assets/fonts/vl_booster_nextfy_black.ttf',
+  './assets/fonts/vl_booster_nextfy_think.ttf',
+  './assets/images/wheel.png',
+  './assets/images/bg-muiten.png',
+  './assets/images/btn2.png',
+  './assets/images/muiten.png',
+  './assets/images/bg.jpg',
+  './assets/js/jquery-3.4.1.min.js',
+  './assets/js/app.js',
+  './assets/js/main.js',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
