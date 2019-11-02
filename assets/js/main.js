5a8ba214471b9c35054a850dfe92765d3f7c5b56
luonghuynh <luonghuynh2525@gmail.com>
2019-11-02 16:51:11 +0700

    commit file len

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,6 @@ window.onload = () => {
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('./sw.js');
+             .register('https://luonghuynh227.github.io/vong-quay-an-nhau/sw.js');
   }
 }
