163e124546599a2f6b0c056f14577918ceecc4a1
luonghuynh <luonghuynh2525@gmail.com>
2019-11-04 14:31:31 +0700

    commit file len

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ window.onload = () => {
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('/sw-vongquay.js');
+             .register('sw-vongquay.js');
   }
 }
