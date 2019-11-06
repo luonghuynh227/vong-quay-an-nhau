c34c62c380464dddedd0541c149e2f9e6c1142dd
luonghuynh <luonghuynh2525@gmail.com>
2019-11-06 17:26:43 +0700

    commit file len

--- sw-vongquay.js
+++ sw-vongquay.js
@@ -1,8 +1,8 @@
 var cacheName = 'hoangtumoi';
 var filesToCache = [
   // '/',
-  'index.html?v=06112019',
-  'assets/css/main.css?v=06112019',
+  'index.html?v=0611201902',
+  'assets/css/main.css?v=0611201902',
   'assets/fonts/vl_booster_nextfy_black.ttf',
   'assets/fonts/vl_booster_nextfy_think.ttf',
   'assets/images/wheel.png',
@@ -11,7 +11,7 @@ var filesToCache = [
   'assets/images/muiten.png',
   'assets/images/bg.jpg',
   'assets/js/jquery-3.4.1.min.js',
-  'assets/js/main.js?v=06112019'
+  'assets/js/main.js?v=0611201902'
 ];
 
 /* Start the service worker and cache all of the app's content */
@@ -28,6 +28,20 @@ self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  // var filesToCache = ['v2'];
+
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (filesToCache.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
     })
   );
 });
